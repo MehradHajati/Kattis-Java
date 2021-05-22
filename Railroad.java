eb708ab41ad53a565f879c136b19e087f21cfe07
MehradHajati <79949480+MehradHajati@users.noreply.github.com>
2021-05-22 00:54:51 -0300

    Add files via upload

---
+++ Railroad.java
@@ -0,0 +1,18 @@
+// Kattis, Railroad
+// https://open.kattis.com/problems/railroad2
+
+import java.util.*;
+
+public class Railroad{
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+        int x = sc.nextInt();
+        int y = sc.nextInt();
+        if(y % 2 == 0){
+            System.out.println("possible");
+        }
+        else{
+            System.out.println("impossible");
+        }
+    }
+}
