489f6aa8043d373bb9332c9159b4d6b5cdb6520f
MehradHajati <79949480+MehradHajati@users.noreply.github.com>
2021-06-09 19:12:34 -0300

    Add files via upload

---
+++ AddTwoNumbers.java
@@ -0,0 +1,13 @@
+// Kattis, AddTwoNumbers
+// https://open.kattis.com/problems/addtwonumbers
+
+import java.util.Scanner;
+
+public class AddTwoNumbers{
+    public static void main(String [] args){
+        Scanner sc = new Scanner(System.in);
+        int a = sc.nextInt();
+        int b = sc.nextInt();
+        System.out.println(a + b);
+    }
+}
